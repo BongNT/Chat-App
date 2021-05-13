caddaf4d42e9df70a154db9be59faa74cb3d7f37
Đinh Hoàng <62587238+HoangDinh161@users.noreply.github.com>
2021-05-13 14:08:18 +0700

    Update Chat_Client.java

--- src/Chat_Client.java
+++ src/Chat_Client.java
@@ -36,7 +36,7 @@ public class Chat_Client extends JFrame {
     }
 
     private void initComponent() {
-        start();
+        //start();
 
         setSize(550,550);
         setResizable(false);
@@ -58,7 +58,44 @@ public class Chat_Client extends JFrame {
         showMsg.setColumns(30);
         showMsg.setRows(25);
 
+        // Test fram chat rieng
+        DefaultListModel<String> l1 = new DefaultListModel<>();
+        String a = "Client2";
+        l1.addElement(a);
+        listClient = new JList<>(l1);
+
         scroll1 = new JScrollPane(this.listClient);
+        scroll1.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scroll1.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        listClient.getSelectionModel().addListSelectionListener(e -> {
+            JFrame private_chat = new JFrame();
+            private_chat.setSize(350,350);
+
+            JTextArea box_msg = new JTextArea();
+            box_msg.setEditable(false);
+            box_msg.setRows(15);
+            box_msg.setColumns(15);
+            JTextArea enterMsg = new JTextArea();
+            enterMsg.setRows(3);
+            enterMsg.setColumns(12);
+
+            JButton send = new JButton("Send");
+            JScrollPane scr1 = new JScrollPane(box_msg);
+            JScrollPane scr2 = new JScrollPane(enterMsg);
+
+            GroupLayout layout = new GroupLayout(private_chat.getContentPane());
+            private_chat.getContentPane().setLayout(layout);
+            layout.setHorizontalGroup(layout.createParallelGroup().addComponent(scr1)
+                                  .addGroup(layout.createSequentialGroup().addComponent(scr2).addGap(20).addComponent(send))
+            );
+            layout.setVerticalGroup(layout.createSequentialGroup().addComponent(scr1).addGap(10)
+                               .addGroup(layout.createParallelGroup().addComponent(scr2).addComponent(send))
+            );
+            private_chat.setVisible(true);
+        });
+
+
         scroll2 = new JScrollPane(this.showMsg);
         scroll2.setViewportView(this.showMsg);
         JScrollPane scroll3 = new JScrollPane(message);
@@ -77,18 +114,18 @@ public class Chat_Client extends JFrame {
         jPanel2 = new JPanel();
         GroupLayout grLayout2 = new GroupLayout(jPanel2);
         grLayout2.setHorizontalGroup(grLayout2.createParallelGroup().addComponent(scroll2)
-                                .addGroup(grLayout2.createParallelGroup(BASELINE).addComponent(scroll3).addComponent(btnSend))
+                                .addGroup(grLayout2.createSequentialGroup().addComponent(scroll3).addComponent(btnSend))
         );
         grLayout2.setVerticalGroup(grLayout2.createSequentialGroup().addComponent(scroll2)
                 .addGap(20)
-                .addGroup(grLayout2.createSequentialGroup().addComponent(scroll3).addGap(10).addComponent(btnSend))
+                .addGroup(grLayout2.createParallelGroup(BASELINE).addComponent(scroll3).addGap(10).addComponent(btnSend))
         );
 
         GroupLayout layout = new GroupLayout(this.getContentPane());
         getContentPane().setLayout(layout);
         layout.setAutoCreateGaps(true);
         layout.setAutoCreateContainerGaps(true);
-        layout.setHorizontalGroup(layout.createSequentialGroup().addComponent(jPanel1,-1,150,-1).addComponent(jPanel2,-1,400,Short.MAX_VALUE));
+        layout.setHorizontalGroup(layout.createSequentialGroup().addComponent(jPanel1,-1,150,-1).addComponent(jPanel2,-1,400,-1));
         layout.setVerticalGroup(layout.createParallelGroup(BASELINE).addComponent(jPanel2).addComponent(jPanel1));
 
 
